f8b714677b1e84783f794095cf5f32b39b0658d8
Seohyeon Kang <seohyeonkang@Seohyeonui-MacBookPro.local>
2020-03-10 19:08:56 +0900

    0310

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: skang <skang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 20:45:20 by skang             #+#    #+#             */
-/*   Updated: 2020/03/03 20:46:16 by skang            ###   ########.fr       */
+/*   Updated: 2020/03/10 16:16:36 by skang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-int	get_next_line(int fd, char **line);
+#include <stdlib.h>
+#include <unistd.h>
 
+int	get_next_line(int fd, char **line);
 
 #endif
